f0ac68d6024afaaad8157ce9180c8e7b6e05ceab
Brett <brettdansmith@gmail.com>
2024-07-16 18:46:28 -0700

    Add TODO for Human in the Loop functionality

--- main.py
+++ main.py
@@ -184,3 +184,6 @@ from helper import ewriter, writer_gui
 MultiAgent = ewriter()
 app = writer_gui(MultiAgent.graph)
 app.launch()
+
+# TODO: Add Human in the Loop functionality with the GUI
+# The question tab should have a response box where a user can answer the question and it will update the task key
